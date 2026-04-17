b3166e4d5f706edb6fe0b5ef9c9deab48698024a
Misbahul Alam <88716007+misbahul-alam@users.noreply.github.com>
2026-04-17 17:05:39 +0600

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+            if (freq == 0) {
+                ans = num;
+            }
+
+            if (ans == num)
+                freq++;
+            else
+                freq--;
+        }
+        return ans;
+    }
+};
