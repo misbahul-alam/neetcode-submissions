eb9f82f4bf40d4d53bd7a561bc9758da2ef5c29c
Misbahul Alam <88716007+misbahul-alam@users.noreply.github.com>
2026-06-10 13:40:10 +0600

    Add: combination-sum-iv - submission-1

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned long long> dp(target + 1, 0);
+
+        dp[0] = 1;
+
+        for (int t = 1; t <= target; t++) {
+            for (int num : nums) {
+                if (t >= num)
+                    dp[t] += dp[t - num];
+            }
+        }
+
+        return dp[target];
+    }
+};
