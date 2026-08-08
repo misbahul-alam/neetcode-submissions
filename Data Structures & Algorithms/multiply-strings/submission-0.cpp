19eb076782acbb0d76cce16f0c1efafddca05142
Misbahul Alam <88716007+misbahul-alam@users.noreply.github.com>
2026-08-08 22:54:38 +0600

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0")
+            return "0";
+
+        int m = num1.size();
+        int n = num2.size();
+        vector<int> result(m + n, 0);
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        string ans;
+        bool leadingZero = true;
+
+        for (int digit : result) {
+            if (digit == 0 && leadingZero)
+                continue;
+
+            leadingZero = false;
+            ans.push_back(digit + '0');
+        }
+
+        return ans.empty() ? "0" : ans;
+    }
+};
